5f746f35a1c34338f165a505aad67030ecd1dbb6
hackingyseguridad <antonio_taboada@hotmail.es>
2025-08-12 23:22:32 +0200

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -4,6 +4,10 @@
 # http://www.hackingyseguridad.com/
 #
 #
+
+
+echo "..."
+echo 
 nmap -v0 -iL ip.txt -F --script=ssl-enum-ciphers -PE -sTV --open -n --randomize-hosts --max-retries 2 --min-rate 999 -oN resultado.txt
 echo "cifrados debiles"
 echo "SSLv2  EXPORT LOW  DES  RC4  MD5 NULL"
