b534c2e24f3e1d4209f99369df6015998de129a3
hackingyseguridad <antonio_taboada@hotmail.es>
2025-08-13 21:57:57 +0200

    Create filtro.sh

---
+++ filtro.sh
@@ -0,0 +1,33 @@
+echo "====================================="
+
+current_ip=""
+vulnerable_found=0
+while read line; do
+  case "$line" in
+    "Nmap scan report for "*)
+      if [ $vulnerable_found -eq 1 ]; then
+        echo ""
+      fi
+      current_ip="$line"
+      vulnerable_found=0
+      ;;
+    *)
+      case "$line" in
+          *SSLv2*|*SSLv3|*TLSv1.0*|*EXPORT*|*LOW*|*DES*|*RC4*|*MD5*|*SWEET32*|*3DES*|*_DH*|*_DHE_*|*TLS_DHE_NULL_*)
+          if [ $vulnerable_found -eq 0 ]; then
+            echo "$current_ip"
+            vulnerable_found=1
+          fi
+          echo "  $line"
+          ;;
+      esac
+      ;;
+  esac
+done < "resultado.txt"
+
+if [ $vulnerable_found -eq 1 ]; then
+  echo ""
+fi
+
+exit 0
+
