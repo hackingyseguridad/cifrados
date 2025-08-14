c75d7a4095d68f3e3bd3f18854cbbf300c2cd7db
hackingyseguridad <antonio_taboada@hotmail.es>
2025-08-14 10:32:24 +0200

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -42,7 +42,7 @@ while read line; do
       ;;
     *)
       case "$line" in
-          *SSLv2*|*SSLv3|*TLSv1.0*|*TLS1.1*|*EXPORT*|*LOW*|*DES*|*RC4*|*MD5*|*SWEET32*|*3DES*|*_DH*|*_DHE_*|*TLS_DHE_NULL_*|*_anon_*)
+         *SSLv2*|*SSLv3|*TLSv1.0*|*TLSv1.1*|*TLSv1.2*|*EXPORT*|*LOW*|*DES*|*RC4*|*_MD5*|*SWEET32*|*3DES*|*_DH*|*_DHE_*|*TLS_DHE_NULL_*|*_anon_*|*_RC2_*)
           if [ $vulnerable_found -eq 0 ]; then
             echo "$current_ip"
             vulnerable_found=1
